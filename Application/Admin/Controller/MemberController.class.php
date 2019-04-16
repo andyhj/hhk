b27b14c9303dcf57f9839d51a2d99a4cdc386a02
andy <952787667@qq.com>
2019-04-16 09:36:10 +0800

    添加vip记录

--- Application/Admin/Controller/MemberController.class.php
+++ Application/Admin/Controller/MemberController.class.php
@@ -5,6 +5,7 @@ class MemberController extends CommonController {
         $current_page = (int)I('p',1);
         $view_datas['search_key'] = $search_key = I('search_key');
         $m_user = D("User");
+        $user_vip_model = M("user_vip");
         $where = [];
         if($search_key){
             $where_s['id']  = array('like', "%{$search_key}%");
@@ -16,7 +17,23 @@ class MemberController extends CommonController {
         $per_page = 15;//每页显示条数
         $page       = getpage($count,$per_page);// 实例化分页类 传入总记录数和每页显示的记录数
         $showPage       = $page->show();// 分页显示输出
-        $view_datas['list'] = $m_user->where($where)->order("add_time DESC")->page($current_page.','.$per_page)->select();
+        $user_list = $m_user->where($where)->order("add_time DESC")->page($current_page.','.$per_page)->select();
+        if($user_list){
+            foreach ($user_list as $value) {
+                $user_vip_info = $user_vip_model->where(["u_id"=>$value['u_id']])->find();
+                $is_plus = '否';
+                $vip_end_time = '';
+                //判断是否plus会员
+                if($user_vip_info && strtotime($user_vip_info["end_time"])> time()){
+                    $is_plus = '是';
+                    $vip_end_time = $user_vip_info["end_time"];
+                }
+                $value['is_plus'] = $is_plus;
+                $value['vip_end_time'] = $vip_end_time;
+                $view_datas['list'][] = $value;
+            }
+        }
+        
         $admin_info = $_SESSION['my_info'];
         $view_datas['num'] = $count;
         $this->assign("admin_info", $admin_info);
@@ -24,8 +41,36 @@ class MemberController extends CommonController {
         $this->assign("page", $showPage);
         $this->display("memberlist");
     }
-    
-    
+    /**
+     * vip记录
+     */
+    public function plusdes(){
+        $user_vip_log_m = M("user_vip_log");
+        $m_user = D("User");
+        $current_page = (int)I('p',1);
+        $view_datas['search_key'] = $search_key = I('search_key');
+        $where = [];
+        if($search_key){
+            $where['u_id'] = $search_key;
+        }
+        $count = $user_vip_log_m->where($where)->count();
+        $per_page = 15;//每页显示条数
+        $page       = getpage($count,$per_page);// 实例化分页类 传入总记录数和每页显示的记录数
+        $showPage       = $page->show();// 分页显示输出
+        $user_vip_log_list = $user_vip_log_m->where($where)->order("add_time DESC")->page($current_page.','.$per_page)->select();
+        if($user_vip_log_list){
+            foreach ($user_vip_log_list as $value) {
+                $user_info = $m_user->getUserOne($value['u_id']);
+                $value['login_id'] = $user_info['login_id'];
+                $value['u_name'] = $user_info['u_name'];
+                $view_datas['list'][] = $value;
+            }
+        }
+        $view_datas['num'] = $count;
+        $this->assign("datas", $view_datas);
+        $this->assign("page", $showPage);
+        $this->display();
+    }
     public function sendvip(){
         $user_id = (int)I('u_id');
         $url = U( 'member/index/');
