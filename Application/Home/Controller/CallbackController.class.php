85cb5cab5a3eeb868489f86e3594ae0ca6d31890
andy <952787667@qq.com>
2019-04-19 09:58:08 +0800

    no message

--- Application/Home/Controller/CallbackController.class.php
+++ Application/Home/Controller/CallbackController.class.php
@@ -54,6 +54,7 @@ class CallbackController extends InitController {
         $plan_model->where(["id"=>$plan_des_info["p_id"]])->save(["status"=>$plan_status]);
 //        $r_s = $plan_des_model->where(["id"=>$plan_des_info["id"]])->save(["order_state"=>1,"d_time"=> $time,"message"=>"消费成功"]);
         $r_s = M()->execute("update __PREFIX__plan_des set order_state=1,d_time={$time},message='消费成功' where id=".$plan_des_info["id"]);
+        add_log("callback_helipay.log", "callback", "执行SQL：".M()->getLastSql());
         add_log("callback_helipay.log", "callback", "更新后数据：".var_export($plan_des_model->where(['id'=>$plan_des_info["id"]])->find(),true));
         add_log("callback_helipay.log", "callback", "执行SQL：".M()->getLastSql());
         add_log("callback_helipay.log", "callback", "执行SQL状态：".$r_s);
