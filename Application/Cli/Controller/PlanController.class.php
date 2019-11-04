0ef562b60c2ed868d75ed2f96657321ccf5fa42c
andy <952787667@qq.com>
2019-11-04 08:44:15 +0800

    no message

--- Application/Cli/Controller/PlanController.class.php
+++ Application/Cli/Controller/PlanController.class.php
@@ -512,8 +512,8 @@ class PlanController extends InitController {
         $hhk_sum_today = $plan_des_info1?$plan_des_info1:0; //会还款今日交易额
 
 
-        $pay_records_m->where("state = 1 and created > '".$date_tod."' and channelId=65 and pay>5000")->save(['channelId'=>0]);
-        $pay_records_m->where("state = 1 and created > '".$date_tod."' and channelId=67 and pay>6000")->save(['channelId'=>0]);
+        $pay_records_m->where("state = 1 and created > '".$date_tod."' and channelId=65 and pay>=5000")->save(['channelId'=>0]);
+        $pay_records_m->where("state = 1 and created > '".$date_tod."' and channelId=67 and pay>=5000")->save(['channelId'=>0]);
         M("plan")->where("c_id=3 and status=1 and amount>10000")->save(['c_id'=>0]);
         
         $je = (int)(strtotime(date('Ymd'))/778);
